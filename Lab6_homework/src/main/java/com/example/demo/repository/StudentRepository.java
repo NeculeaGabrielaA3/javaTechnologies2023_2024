bd86da404a1e1d0747addf512972e2fc50158726
Gabriela Neculea <79145195+NeculeaGabrielaA3@users.noreply.github.com>
2023-12-03 15:37:30 +0200

    second commit homework 6

--- Lab6_homework/src/main/java/com/example/demo/repository/StudentRepository.java
+++ Lab6_homework/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Project;
 import com.example.demo.model.Student;
 
 import javax.ejb.Stateless;
@@ -29,6 +30,24 @@ public class StudentRepository {
     public List<Student> findAll() {
         return entityManager.createNamedQuery("Student.findAll", Student.class)
                 .getResultList();
+    }
+    @Transactional
+    public void deleteById(Long studentId) {
+        Student student = entityManager.find(Student.class, studentId);
+        if(student != null) {
+            // Find all projects assigned to this student and remove the association
+            List<Project> projects = entityManager.createQuery("SELECT p FROM Project p WHERE p.student.id = :studentId", Project.class)
+                    .setParameter("studentId", studentId)
+                    .getResultList();
+
+            for (Project project : projects) {
+                project.setStudent(null); // Remove the student reference
+                entityManager.merge(project); // Update the project
+            }
+
+            // Now delete the student
+            entityManager.remove(student);
+        }
     }
 
     public void setEntityManager(EntityManager em) {
